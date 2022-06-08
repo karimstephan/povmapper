165893078c25bbb9f35a743b2b9d191d092cc855
Annuka Bathija <annuka.bathija@gmail.com>
2022-06-08 11:50:12 +0800

    first draft of frontend - to be adapted to model

--- frontend.py
+++ frontend.py
@@ -1,34 +1,45 @@
-import pandas as pd
 import streamlit as st
-
-df = pd.read_csv('processed_data/processed_data.csv')
-df = df[['LATNUM', 'LONGNUM']]
-df.rename(columns = {'LONGNUM': 'lon', 'LATNUM': 'lat' }, inplace = True)
-st.map(df)
-
-'''
-import streamlit as st
-from shapely.geometry import Point, Polygon
-import geopandas as gpd
 import pandas as pd
-import geopy
-
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import Fullscreen
 
-street = st.sidebar.text_input("Street", "75 Bay Street")
-city = st.sidebar.text_input("City", "Toronto")
-province = st.sidebar.text_input("Province", "Ontario")
-country = st.sidebar.text_input("Country", "Canada")
 
+token = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ'  # your mapbox token
+tileurl = 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=' + str(token)
+
+
+#User input
+#don't forget to mention in description for users that it will take the highest level of admin levels
+
+street = st.sidebar.text_input("Street")
+city = st.sidebar.text_input("City")
+province = st.sidebar.text_input("Province")
+country = st.sidebar.text_input("Country")
+
+# geolocator
 geolocator = Nominatim(user_agent="GTA Lookup")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 location = geolocator.geocode(street+", "+city+", "+province+", "+country)
+#in the case there is no input from user, to avoid error we added a conditional
+if location is not None:
+    lat = location.latitude
+    lon = location.longitude
+#introducing folium
+    m = folium.Map(location=[lat,lon],
+                zoom_start=10,
+                tiles=tileurl,
+                attr='Mapbox',
+                    control_scale=False)
+    Fullscreen().add_to(m)
+ #adding folium marker   
+    folium.Circle(
+    radius=5000,
+    location=[lat, lon],
+    popup="poverty index =",
+    color="red",
+    fill=True,).add_to(m)
+    folium_static(m)
 
-lat = location.latitude
-lon = location.longitude
-
-map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
-
-st.map(map_data, zoom=12)
-'''
